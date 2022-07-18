56767032395ffba1ab5778bb3bc07e6b18adb2bf
Reda El Bettioui <74994440+XredaX@users.noreply.github.com>
2022-07-18 06:30:39 -0700

    Add files via upload

---
+++ remember.py
@@ -0,0 +1,16 @@
+import schedule
+import requests
+
+
+def send_msg():
+   url="https://api.telegram.org/bot5102702091:AAFI9FISwdHRjdQYLPxkcD2DGpWnQHYqwbg/sendMessage?chat_id=-1001654861712&parse_mode=Markdown&text="
+   request = url+"Start"
+   requests.get(request)
+
+def job1():
+    send_msg()
+
+print("Start remember")
+schedule.every().day.at("05:48").do(job1)
+while True:
+   schedule.run_pending()
